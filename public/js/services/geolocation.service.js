5caae1d0c1fe5c2a2693bc5003be6999a83fdd3a
Cole Willsea <coleww@gmail.com>
2016-10-27 19:05:53 -0700

    create service to get geolocation data

---
+++ public/js/services/geolocation.service.js
@@ -0,0 +1,34 @@
+(function () {
+  'use strict';
+
+
+  angular.module('weatherApp', []).factory('GeoLocationService', ['$q', '$window', function ($q, $window) {
+    var seattleCoords = {
+      latitude: '47.6062',
+      longitude: '122.3321'
+    };
+
+    function getLocation () {
+      var deferred = $q.defer();
+
+      if (!$window.navigator.geolocation) {
+        // browser does not support geolocation, just return coords for Seattle
+        deferred.resolve(seattleCoords);
+      } else {
+        $window.navigator.geolocation.getCurrentPosition(function (position) {
+          // user allowed geolocation, return object containing latitude/longitude
+          deferred.resolve(position);
+        }, function (err) {
+          // user did not allow geolocation access, or something went wrong, so just use Seattle
+          deferred.resolve(seattleCoords);
+        });
+      }
+
+      return deferred.promise;
+    }
+
+    return {
+      getLocation: getLocation
+    }
+  }]);
+})();
--- public/js/services/geolocation.service.spec.js
+++ public/js/services/geolocation.service.spec.js
@@ -1,5 +1,77 @@
 describe('GeoLocationService', function() {
-  it('can add', function() {
-    expect(2 + 2).toEqual(4);
+  var GeoLocationService;
+  var $scope;
+  var $window;
+
+  // spying on the success/error style callback that geolocation uses is super weird >_<
+  var successCB;
+  var errorCB;
+
+  var fakeLocationData = {
+    latitude: '123.456',
+    longitude: '789.10'
+  };
+
+  var seattleCoords = {
+    latitude: '47.6062',
+    longitude: '122.3321'
+  };
+
+  var fakeWindow = {
+    navigator: {
+      geolocation: {
+        getCurrentPosition: function (success, error) {}
+      }
+    }
+  };
+
+  beforeEach(function () {
+    module('weatherApp');
+
+    module(function ($provide) {
+      $provide.value('$window', fakeWindow);
+    });
+
+    spyOn(fakeWindow.navigator.geolocation, 'getCurrentPosition').and.callFake(function (success, error) {
+      // store off the success/error callbacks for our mocked geolocation when it is called
+      // this way we can manually trigger these callbacks and pass in mocked position data
+      successCB = success;
+      errorCB = error;
+    });
+  });
+  beforeEach(angular.mock.inject(function (_GeoLocationService_, _$rootScope_, _$window_) {
+    GeoLocationService = _GeoLocationService_;
+    $scope = _$rootScope_;
+  }));
+
+  it('returns a promise that resolves to a location if geolocation is enabled', function() {
+    var result;
+    GeoLocationService.getLocation().then(function (data) {
+      result = data;
+    })
+    successCB(fakeLocationData);
+    $scope.$apply(); // this triggers a digest cycle, which ensures the promise has been resolved.
+    expect(result).toEqual(fakeLocationData);
+  });
+
+  it('returns a promise that resolves to Seattle coords if geolocation fails', function() {
+    var result;
+    GeoLocationService.getLocation().then(function (data) {
+      result = data;
+    })
+    errorCB();
+    $scope.$apply();
+    expect(result).toEqual(seattleCoords);
+  });
+
+
+  it('returns a promise that resolves to Seattle coords if geolocation is unavailable', function() {
+    var result;
+    fakeWindow.navigator = {};
+    GeoLocationService.getLocation().then(function (data) {
+      result = data;
+    })
+    $scope.$apply();
+    expect(result).toEqual(seattleCoords);
   });
 });
