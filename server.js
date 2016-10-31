0c533dc41b28d7f1b3e68cf3338bce8cdb278082
Cole Willsea <coleww@gmail.com>
2016-10-31 10:56:02 -0700

    add https redirect

--- server.js
+++ server.js
@@ -3,17 +3,24 @@ var app = express();
 var path = require('path');
 var port = process.env.PORT || 3000;
 var request = require('request');
+var httpsRedirect = require('express-https-redirect');
+
 var createDarkSkyUrl = require('./utils/createDarkSkyUrl');
 
 var sampleResponse = require('./sampleResponse.json');
 
 app.set('port', port);
+
+// this will redirect HTTP requests to HTTPS, which we need in order to access the geolocation API
+app.use('/', httpsRedirect());
+
+// serve up our app code from /public
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 var server = app.listen(app.get('port'), function() {
   console.log('Express running on ' + port);
 });
-
 
 app.get('/api', function(req,res) {
   // TODO: check that lat/lon are passed as query params, otherwise return an error
